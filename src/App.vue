98fdaa75616cdd942876dbde5e4b94d9c0994b5c
Nguyen Tien Dung <63250067+dungnt2khust@users.noreply.github.com>
2022-03-14 11:03:00 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" width="100px" />
+    <div
+      class="fb-login-button"
+      data-width=""
+      data-size="large"
+      data-button-type="continue_with"
+      data-layout="default"
+      data-auto-logout-link="false"
+      data-use-continue-as="false"
+    ></div>
+  </div>
+</template>
+
+<script>
+// import { FB as VFacebookLogin } from "vue-facebook-login-component";
+
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
+<style >
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
